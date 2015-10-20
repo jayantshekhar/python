775e26e560cb42e82136a6fed42e9c26b5fe84a0
Jayant Shekar <jayant@user-MBPMBA-3.local>
2015-10-20 11:10:35 -0700

    fibonacci

---
+++ src/fibonacci.py
@@ -0,0 +1,8 @@
+__author__ = 'jayant'
+
+parents, babies = (1, 1)
+while babies < 100:
+    print 'This generation has {0} babies'.format(babies)
+    parents, babies = (babies, parents + babies)
+
+    
